c939f91288c8cece75dbfb11eb283258a203d902
Collin Brooks <collin.brooks@gmail.com>
2010-12-17 16:18:03 -0600

    Modules now consist of lowercase names. Files renamed to reflect this change.

--- src/beforeeffects.jsx
+++ src/beforeeffects.jsx
@@ -1,21 +1,23 @@
 /**
-* @fileoverview <code>BE</code> (BeforeEffects) aims to be a collection of
-* useful extensions to the After Effects Object Model within ExtendScript.
-* <code>BE</code> contains Objects that provide greater functionality and
-* reusability than the built-in After Effects objects as well as prototyple
-* modifications to the built-in Objects within JavaScript.
-* 
-* All prototype modifications to the built-in JavaScript Objects are handled
-* gently by checking to make sure the methods/fields that are added are not
-* already defined. This is helpful in that, if the standard Objects are modified
-* by future ECMA specifications (or ExtendScript is modified) and they are named
-* the same as methods/fields that <code>BE</code> implements, BeforeEffects will
-* not overwrite the pre-defined methods. 
-* 
-* @author <a href="mailto:collin.brooks@gmail.com">Collin Brooks</a>
-* @version 0.1.0 dev
-*/
+ * @fileoverview <code>BE</code> (BeforeEffects) aims to be a collection of
+ * useful extensions to the After Effects Object Model within ExtendScript.
+ * <code>BE</code> contains Objects that provide greater functionality and
+ * reusability than the built-in After Effects objects as well as prototyple
+ * modifications to the built-in Objects within JavaScript.
+ * 
+ * All prototype modifications to the built-in JavaScript Objects are handled
+ * gently by checking to make sure the methods/fields that are added are not
+ * already defined. This is helpful in that, if the standard Objects are
+ * modified by future ECMA specifications (or ExtendScript is modified) and they
+ * are named the same as methods/fields that <code>BE</code> implements,
+ * BeforeEffects will not overwrite the pre-defined methods. 
+ * 
+ * @author <a href="mailto:collin.brooks@gmail.com">Collin Brooks</a> @version
+ * 0.1.0 dev
+ */
 
+/*jslint white: true, onevar: true, undef: true, nomen: true, eqeqeq: true,
+  plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
 
 /**
  * The <code>BE</code> Object aims to be an all-purpose collection of useful
@@ -27,24 +29,24 @@
  */
 var BE = (function ()
 {
-    /*****************************************************************************\
+    /**************************************************************************\
     * >>> Private Variables
-	\*****************************************************************************/
+	\**************************************************************************/
 	
     /**
-    * A reference to this function's 'this' value for use within
-    * methods/functions defined by the BE Object.
-    * 
-    * @type {Object}
+     * A reference to this function's 'this' value for use within
+     * methods/functions defined by the BE Object.
+     * 
+     * @type {Object}
      */
     
     var that = this,
 
-	/**
-	 *The current version of the BE object.
+    /**
+     * The current version of the BE object.
 	 *
-	 *@type {String}
-	 *@private
+	 * @type {String}
+	 * @private
 	 */
 	version = "0.1.0 dev",
 
@@ -62,39 +64,40 @@ var BE = (function ()
 	debug = 3,
 
     /**
-    * The base folder, relative to this file, where the "inc" folder can be
-    * found.
-    * @type {Folder}
-    */
+     * The base folder, relative to this file, where the "inc" folder can be
+     * found.
+     * @type {Folder}
+     */
     includeFolder = new Folder(new File($.fileName).parent.fsName +
         "/inc").fsName, 
 
     /**
-    * An array of filenames, relative to the inc/ folder, where the
-    * modules/languages can be found.
-    * @type {Array}
-    */
+     * An array of filenames, relative to the inc/ folder, where the
+     * modules/languages can be found.
+     * @type {Array}
+     */
     includes = [
         // Modules
         "/modules/BE_prototypes.jsxinc",
-        "/modules/BE_PROJ_helper.jsxinc",
-        "/modules/BE_COMP_helper.jsxinc",
-        "/modules/BE_LAYER_helper.jsxinc",
-        "/modules/BE_TIME_helper.jsxinc",
-        "/modules/BE_UI_helper.jsxinc"
+        "/modules/BE_proj.jsxinc",
+        "/modules/BE_comp.jsxinc",
+        "/modules/BE_layer.jsxinc",
+        "/modules/BE_time.jsxinc",
+        "/modules/BE_ui.jsxinc",
+        "/modules/BE_SettingsManager.jsxinc"
         //Languages
     ];
     
-	/*****************************************************************************\
+	/**************************************************************************\
 	* >>> Public Methods
-	\*****************************************************************************/
-	
+	\**************************************************************************/
+
 	/**
      * Contains methods that help with debugging a script
 	 * @memberOf BE
 	 * @namespace
      */
-    this.LOG = {};
+    this.log = {};
 
 	/**
 	 * Log message type contstants. Values:
@@ -104,9 +107,9 @@ var BE = (function ()
      * <li>DEBUG</li>
      * </ul>
      * @constant
-     * @see BE.LOG
+     * @see BE.log
 	 */
-	this.LOG.types = {
+	this.log.types = {
 		EXCEPTION: "FATAL ERROR: ",
 		WARN: "WARNING: ",
         DEBUG: "INFO: "
@@ -114,33 +117,32 @@ var BE = (function ()
 
 	/**
      * Writes a message out to the ExtendScript console. The individual
-     * <code>BE.LOG.debug</code>, <code>BE.LOG.warn</code> and
+     * <code>BE.log.debug</code>, <code>BE.log.warn</code> and
      * <code>BE.log.error</code> can be used as short forms for the
      * functionality of this method.
      *
-     * @param {BE.LOG.types} type The <code>BE.LOG.type</code> to use for this
+     * @param {BE.log.types} type The <code>BE.log.type</code> to use for this
      * log message
      * @param {String} message The message to log
 	 */
-    this.LOG.write = function (type, message)
-    {
+    this.log.write = function (type, message) {
 		//Make sure the debug level is at least one greater than zero.
-        if (that.debug > 0)
+        if (debug > 0)
         {
             switch (type) {
-            case that.LOG.types.DEBUG:
-                if (BE.debug >= 1)
+            case that.log.types.DEBUG:
+                if (debug >= 1)
                 {
                     $.writeln(type + message);
                 }
                 break;
-            case that.LOG.types.WARN:
-                if (that.debug >= 2)
+            case that.log.types.WARN:
+                if (debug >= 2)
                 {
                     $.writeln(type + message);
                 }
                 break;
-            case that.LOG.types.EXCEPTION:
+            case that.log.types.EXCEPTION:
                 $.writeln(type + message);
                 that.alertError("" + type + message);
                 break;
@@ -155,24 +157,24 @@ var BE = (function ()
      * Outputs a warning message to the console.
      * @param {String} message The message to display
      */
-    this.LOG.warn = function (message) {
-        that.LOG.write(that.LOG.types.WARN, message);
+    this.log.warn = function (message) {
+        that.log.write(that.log.types.WARN, message);
     };
 
     /**
      * Outputs an info message to the console.
      * @param {String} message The message to display
      */
-    this.LOG.debug = function (message) {
-        that.LOG.write(that.LOG.types.DEBUG, message);
+    this.log.debug = function (message) {
+        that.log.write(that.log.types.DEBUG, message);
     };
 
     /**
      * Outputs an error message to the console.
      * @param {Error} e The error to output.
      */
-    this.LOG.error = function (e) {
-        that.LOG.write(that.LOG.types.EXCEPTION, e.toString());
+    this.log.error = function (e) {
+        that.log.write(that.log.types.EXCEPTION, e.toString());
     };
 
 
@@ -182,8 +184,7 @@ var BE = (function ()
      * @param {String} s The name of the script to mention in the log start
      * @returns Nothing.
      */
-    this.LOG.insertLogStart = function (s)
-    {
+    this.log.insertLogStart = function (s) {
         var d = new Date(),
             m = "----------------\n" +
                s + "\n" +
@@ -195,23 +196,23 @@ var BE = (function ()
     /**
      * Inserts a horizontal rule in the log to visually separate the info to be
      * written to the log.
-     * @param {Array|Int} [padding = 0] <p>One of two types of values can be sent as
-     * the parameter for this method. If you pass an Integer or a single value
-     * array, that many lines will be added before and after the separator.</p>
+     * @param {Array|Int} [padding = 0] <p>One of two types of values can be
+     * sent as the parameter for this method. If you pass an Integer or a single
+     * value array, that many lines will be added before and after the
+     * separator.</p>
      * 
      * <p>If you pass a two value array, i.e. [1,2], the Integer at position 0
      * is used to determine how many blank lines to place above the separator
      * and the Integer at position [1] is used to determine how many blank lines
      * to place below the separator.</p>
      * 
-     * <p>If the array sent has a length greater than 2, only the first two indices
-     * are used.</p>
+     * <p>If the array sent has a length greater than 2, only the first two
+     * indices are used.</p>
      * @param {String} [s="-"] The character to use as the separator.
      * @param {Int} [n = 15] How many characters to place within the horizontal
      * separator line.
      */
-    this.LOG.separate = function(padding, s, n) 
-	{
+    this.log.separate = function (padding, s, n) {
 
         var i, padBefore, padAfter, separatorString = "";
 
@@ -227,7 +228,7 @@ var BE = (function ()
                 padBefore = padding[0];
                 padAfter = padding[1];
             }
-        } else if(padding instanceof String) {
+        } else if (padding instanceof String) {
             padBefore = padAfter = parseInt(padding, 10);
         }
 
@@ -250,33 +251,32 @@ var BE = (function ()
     };
 
 	/**
-	* Set the debug level for the Before Effects LOG method.
-	* 
-	* The following levels are available:
-	* <ul>
-	* 	<li>0 - Debug completely off.</li>
-	* 	<li>1 - Information displayed.</li>
-	* 	<li>2 - Warnings shown.</li>
-	* </ul>
-	* 
-    * @param {Int} l The level number to set as the Debug Level.
-	* @example
-    * BE.setDebugLevel(1);
-	* @returns Nothing.
-	* @see BE.LOG
-	*/
-	this.setDebugLevel = function (l)
-	{
+	 * Set the debug level for the Before Effects LOG method.
+	 * 
+	 * The following levels are available:
+	 * <ul>
+	 * 	<li>0 - Debug completely off.</li>
+	 * 	<li>1 - Information displayed.</li>
+	 * 	<li>2 - Warnings shown.</li>
+	 * </ul>
+	 * 
+     * @param {Int} l The level number to set as the Debug Level.
+	 * @example
+     * BE.setDebugLevel(1);
+	 * @returns Nothing.
+	 * @see BE.log
+	 */
+	this.setDebugLevel = function (l) {
 		debug = l;	
 	};
 
     /**
-    * Returns the version string of the current BE Object.
-    * 
-    * @returns {String} The BE Object's version string.
-    */
-    this.getVersion() = function (){
-       return that.version;
+     * Returns the version string of the current BE Object.
+     * 
+     * @returns {String} The BE Object's version string.
+     */
+    this.getVersion = function () {
+        return that.version;
     };
     
 	/**
@@ -289,7 +289,7 @@ var BE = (function ()
 	this.checkCompatability = function () {
 		var aeVersion = (app.version) ? app.version : null;
 
-		if(aeVersion === null) 
+		if (aeVersion === null) 
 		{
 
 		}
@@ -305,10 +305,9 @@ var BE = (function ()
      * within the Error will be used to alert the user.
      * @return Nothing.
      */
-    this.alertError = function (e)
-    {
+    this.alertError = function (e) {
         var errString = "",
-            eWindow
+            eWindow,
 			errStringMaxLength = 100;
 
 		if (e instanceof Error) {
@@ -347,46 +346,49 @@ var BE = (function ()
 		    alert(errString);
 		}
 
-		that.LOG.warn(errString);
+		that.log.warn(errString);
     };
 
     /**
-    * Initializes the module and language includes. To reload a module, you can
-    * pass the name of the module (minus the file extension and "BE_") to the
-    * init method at any time.
-    * @param {String[]} [moduleList] An Array of module names to make available.
-    * If blank, "all" or undefined, all modules will be loaded. If a module
-    * fails to load (either because there is an error within the module itself
-    * or the module doesn't exist) execution halts with an error.
-    * @returns return_type return_description
-    */
-    this.init = function (moduleList){
+     * Initializes the module and language includes. To reload a module, you can
+     * pass the name of the module (minus the file extension and "BE_") to the
+     * init method at any time.
+     * @param {String[]} [moduleList] An Array of module names to make
+     * available.  If blank, "all" or undefined, all modules will be loaded. If
+     * a module fails to load (either because there is an error within the
+     * module itself or the module doesn't exist) execution halts with an error.
+     * @returns {Bool} True if te initialization was successful or false if it
+     * wasn't.
+     */
+    this.init = function (moduleList) {
         var inc;
 
         //If the module list is not defined, set it to "all" 
-        moduleList = (!moduleList || moduleList === []) ? that.includes :
+        moduleList = (!moduleList || moduleList === []) ? includes :
         moduleList;
 
-        that.LOG.debug("Num Includes -> " + includes.length);
+        that.log.debug("Num Includes -> " + includes.length);
 
-        for (inc = 0; inc < moduleList.length; inc++)
+        for (inc = 0; inc < moduleList.length; inc += 1)
         {
             try {
-                that.LOG.debug("Including -> " + "[" + inc + "]" + moduleList[inc]);
-                $.evalFile(new File(that.includeFolder + "/" + moduleList[inc]));
+                that.log.debug("Including -> " +
+                    "[" + inc + "]" + moduleList[inc]);
+                $.evalFile(new File(includeFolder + "/" + moduleList[inc]));
             } catch (e) {
                 that.alertError("There was an error while evaluating the " +
-                "includes. The following include failed: " + moduleList[inc]);
+                "includes. The following include failed: " + moduleList[inc] +
+                "\n\nERROR:\n" + e);
 
                 return false;
             }
         }
+        return true;
     };
     
-
-	/*****************************************************************************\
+	/**************************************************************************\
 	* >>> ERROR
-	\*****************************************************************************/
+	\**************************************************************************/
 
     /**
      * Set of string error constants used by the global BE Object.
@@ -398,24 +400,25 @@ var BE = (function ()
      */
     this.ERROR = {
 		/**
-		* An error stating that there is no open After Effects
-		* project.
-		* @type {String}
-		*/
+		 * An error stating that there is no open After Effects
+		 * project.
+		 * @type {String}
+		 */
         NO_OPEN_PROJECT: "An open project could not be found!",
 
         /**
-        * An error stating the active item within the currently open After Effects file cannot be
-        * found.
-        * @type {String}
-        */
+         * An error stating the active item within the currently open After
+         * Effects file cannot be found.
+         * @type {String}
+         */
         NO_ACTIVE_ITEM : "The active item within the project could " +
             "not be found!",
 
         /**
-        * An error stating that the active composition within the currently open project could not be found.
-        * @type {String}
-        */
+         * An error stating that the active composition within the currently
+         * open project could not be found.
+         * @type {String}
+         */
         NO_ACTIVE_COMP : "The active composition within the " +
             "project could not be found!",
     };
