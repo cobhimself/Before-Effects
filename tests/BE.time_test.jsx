042a5b4f31b05bb7fe445ef492ee7dea01593098
Collin Brooks <collin.brooks@gmail.com>
2012-06-17 16:29:06 -0500

    Fixed problems with time.jsxinc based upon unit tests. Also modified unit tests to better the tests.

--- tests/BE.time_test.jsx
+++ tests/BE.time_test.jsx
@@ -9,14 +9,93 @@
 
 TB.module('BE.time');
 BE.require('BE.time');
+BE.require('BE.comp');
 
 TB.test('BE.time properties', function () {
     TB.hasProperty(BE, "time", "time is a property of BE");
     TB.hasProperty(BE.time, "getTimeDisplayTypeString", "BE.time.getTimeDisplayTypeString is a property of BE.time");
     TB.hasProperty(BE.time, "setTimeDisplayType", "BE.time.setTimeDisplayType is a property of BE.time");
-    TB.hasProperty(BE.time, "getTimeInSeconds", "BE.time.getTimeInSeconds is a property of BE.time");
-    TB.hasProperty(BE.time, "getTimeInFrames", "BE.time.getTimeInFrames is a property of BE.time");
     TB.hasProperty(BE.time, "withTimecodeRemembered", "BE.time.withTimecodeRemembered is a property of BE.time");
+});
+
+TB.test('BE.time.setTimeDisplayType()', function () {
+    //Initialize the testConfig comp object
+    testConfig.comp.init();
+
+    var theComp = testConfig.comp.theComp;
+
+    //The tests differ based upon AE version.
+    if (BE.AEVersion < 10.5) {
+        //Set the time display type to frames and see if it worked.
+        BE.time.setTimeDisplayType("frames");
+
+        TB.equal(
+            app.project.timecodeDisplayType,
+            TimecodeDisplayType.FRAMES,
+            "BE.time.setTimeDisplayType(\"frames\") set correctly!"
+        );
+
+        BE.time.setTimeDisplayType("timecode");
+
+        TB.equal(
+            app.project.timecodeDisplayType,
+            TimecodeDisplayType.TIMECODE,
+            "BE.time.setTimeDisplayType(\"timecode\") set correctly!"
+        );
+
+        BE.time.setTimeDisplayType("FRAMES");
+
+        TB.equal(
+            app.project.timecodeDisplayType,
+            TimecodeDisplayType.FRAMES,
+            "BE.time.setTimeDisplayType(\"FRAMES\") set correctly!"
+        );
+
+        BE.time.setTimeDisplayType("TIMECODE");
+
+        TB.equal(
+            app.project.timecodeDisplayType,
+            TimecodeDisplayType.TIMECODE,
+            "BE.time.setTimeDisplayType(\"TIMECODE\") set correctly!"
+        );
+
+    } else {
+        //Set the time display type to frames and see if it worked.
+        BE.time.setTimeDisplayType("frames");
+
+        TB.equal(
+            app.project.timeDisplayType,
+            TimeDisplayType.FRAMES,
+            "BE.time.setTimeDisplayType(\"frames\") set correctly!"
+        );
+
+        BE.time.setTimeDisplayType("timecode");
+
+        TB.equal(
+            app.project.timeDisplayType,
+            TimeDisplayType.TIMECODE,
+            "BE.time.setTimeDisplayType(\"timecode\") set correctly!"
+        );
+
+        BE.time.setTimeDisplayType("FRAMES");
+
+        TB.equal(
+            app.project.timeDisplayType,
+            TimeDisplayType.FRAMES,
+            "BE.time.setTimeDisplayType(\"FRAMES\") set correctly!"
+        );
+
+        BE.time.setTimeDisplayType("TIMECODE");
+
+        TB.equal(
+            app.project.timeDisplayType,
+            TimeDisplayType.TIMECODE,
+            "BE.time.setTimeDisplayType(\"TIMECODE\") set correctly!"
+        );
+    }
+
+    //Clean up the testConfig comp object
+    testConfig.comp.clean();
 });
 
 TB.test('BE.time.getTimeDisplayTypeString()', function () {
@@ -25,53 +104,37 @@ TB.test('BE.time.getTimeDisplayTypeString()', function () {
 
     var theComp = testConfig.comp.theComp;
 
+    //Set the timecode display type to frames for the test.
     if (BE.AEVersion < 10.5) {
         app.project.timecodeDisplayType = TimecodeDisplayType.FRAMES;
-        TB.ok(
-            BE.time.getTimeDisplayTypeString("frames"),
-            "BE.time.getTimeDisplayTypeString('frames') is true."
-        );
-        TB.ok(
-            BE.time.getTimeDisplayTypeString("FRAMES"),
-            "BE.time.getTimeDisplayTypeString('FRAMES') is true."
-        );
-
-        app.project.timecodeDisplayType = TimecodeDisplayType.TIMECODE;
-        TB.ok(
-            BE.time.getTimeDisplayTypeString("timecode"),
-            "BE.time.getTimeDisplayTypeString('timecode') is true."
-        );
-        TB.ok(
-            BE.time.getTimeDisplayTypeString("timecode"),
-            "BE.time.getTimeDisplayTypeString('timecode') is true."
-        );
     } else {
         app.project.timeDisplayType = TimeDisplayType.FRAMES;
-        TB.ok(
-            BE.time.getTimeDisplayTypeString("frames"),
-            "BE.time.getTimeDisplayTypeString('frames') is true."
-        );
-        TB.ok(
-            BE.time.getTimeDisplayTypeString("FRAMES"),
-            "BE.time.getTimeDisplayTypeString('FRAMES') is true."
-        );
-
-        app.project.timeDisplayType = TimeDisplayType.TIMECODE;
-        TB.ok(
-            BE.time.getTimeDisplayTypeString("timecode"),
-            "BE.time.getTimeDisplayTypeString('timecode') is true."
-        );
-        TB.ok(
-            BE.time.getTimeDisplayTypeString("timecode"),
-            "BE.time.getTimeDisplayTypeString('timecode') is true."
-        );
     }
+
+    TB.equal(
+        BE.time.getTimeDisplayTypeString(),
+        "frames",
+        "BE.time.getTimeDisplayTypeString() is frames."
+    );
+
+    //Set the timecode display type to timecode for the tst.
+    if (BE.AEVersion < 10.5) {
+        app.project.timecodeDisplayType = TimecodeDisplayType.TIMECODE;
+    } else {
+        app.project.timeDisplayType = TimeDisplayType.TIMECODE;
+    }
+
+    TB.equal(
+        BE.time.getTimeDisplayTypeString(),
+        "timecode",
+        "BE.time.getTimeDisplayTypeString() is timecode."
+    );
 
     //Clean up the testConfig comp object
     testConfig.comp.clean();
 });
 
-TB.test('BE.time.timecodeDisplayTypeIs()', function () {
+TB.test('BE.time.timeDisplayTypeIs()', function () {
     //Initialize the testConfig comp object
     testConfig.comp.init();
 
@@ -80,18 +143,18 @@ TB.test('BE.time.timecodeDisplayTypeIs()', function () {
 
     //Set the timecode display type to frames and check.
     BE.time.setTimeDisplayType("frames");
-    TB.ok(BE.time.timecodeDisplayTypeIs("frames"), "timecodeDisplayTypeIs(\"frames\") is correct!");
+    TB.ok(BE.time.timeDisplayTypeIs("frames"), "timeDisplayTypeIs(\"frames\") is correct!");
 
     //Set the timecode display type to frames and check.
     BE.time.setTimeDisplayType("FRAMES");
-    TB.ok(BE.time.timecodeDisplayTypeIs("FRAMES"), "timecodeDisplayTypeIs(\"FRAMES\") is correct!");
+    TB.ok(BE.time.timeDisplayTypeIs("FRAMES"), "timeDisplayTypeIs(\"FRAMES\") is correct!");
 
     //Set the timecode display type to timecode and check.
     BE.time.setTimeDisplayType("timecode");
-    TB.ok(BE.time.timecodeDisplayTypeIs("timecode"), "timecodeDisplayTypeIs(\"timecode\") is correct!");
+    TB.ok(BE.time.timeDisplayTypeIs("timecode"), "timeDisplayTypeIs(\"timecode\") is correct!");
 
     BE.time.setTimeDisplayType("TIMECODE");
-    TB.ok(BE.time.timecodeDisplayTypeIs("TIMECODE"), "timecodeDisplayTypeIs(\"TIMECODE\") is correct!");
+    TB.ok(BE.time.timeDisplayTypeIs("TIMECODE"), "timeDisplayTypeIs(\"TIMECODE\") is correct!");
 
     //Clean up the testConfig comp object
     testConfig.comp.clean();
@@ -116,17 +179,38 @@ TB.test('BE.time.setTimeDisplayType()', function () {
     testConfig.comp.clean();
 });
 
-TB.test('BE.comp.withTimecodeRemembered()', function () {
+TB.test('BE.time.withTimecodeRemembered()', function () {
+    var theComp = testConfig.comp.theComp,
+        timeDisplayTypeOutsideStart,
+        timeDisplayTypeInside,
+        timeDisplayTypeOutsideEnd;
+
     //Initialize the testConfig comp object
-    /*testConfig.comp.init();*/
+    testConfig.comp.init();
 
-    /*var theComp = testConfig.comp.theComp,*/
-        /*testLayers = testConfig.comp.testLayers;*/
+    BE.time.setTimeDisplayType("timecode");
+    timeDisplayTypeOutsideStart = BE.time.getTimeDisplayTypeString();
 
-    TB.ok(false, "TO DO!");
+    BE.time.withTimecodeRemembered("frames", function () {
+        timeDisplayTypeInside = BE.time.getTimeDisplayTypeString();
+    });
+
+    timeDisplayTypeOutsideEnd = BE.time.getTimeDisplayTypeString();
+
+    TB.equal(
+        timeDisplayTypeInside,
+        "frames",
+        "Timecode set correctly within inner function."
+    );
+
+    TB.equal(
+        timeDisplayTypeOutsideStart,
+        timeDisplayTypeOutsideEnd,
+        "Timecode remembered correctly."
+    );
 
     //Clean up the testConfig comp object
-    /*testConfig.comp.clean();*/
+    testConfig.comp.clean();
 });
 
 /** TODO: **/
